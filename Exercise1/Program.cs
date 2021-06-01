9ba62e660bbcf4e95e3eefb2a8015536eab37b0f
澁澤虎太郎 <32236@ojs.ac.jp>
2021-06-01 09:43:10 +0900

    i

---
+++ Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            // 2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+
+        }
+
+        // 2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:m\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
+        }
+    }
+}
