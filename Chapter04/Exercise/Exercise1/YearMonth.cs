80c20d2eb7e8261144a625173242d5849f8af4de
澁澤虎太郎 <32236@ojs.ac.jp>
2021-06-10 15:41:20 +0900

    p133 4.1.4

---
+++ Chapter04/Exercise/Exercise1/YearMonth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    //4.1.1
+    public class YearMonth {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int month, int year) {
+            Month = month;
+            Year = year;
+        }
+
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+        //4.1.3
+        public YearMonth AddOneMonth() {
+           
+            if (Month == 12) {
+                return new YearMonth(this.Year + 1, 1);
+            } else {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+
+        }
+        //4.1.4
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = new Product();
+            var product = Session["MyProduct"] as Product;
+            if (product == null) {
+                //Productが取得できなかった時の処理
+                Console.WriteLine("Productが取得できなかった");
+            } else {
+                //Productが取得できた時の処理
+                Console.WriteLine("Productが取得できた");
+            }
+
         }
+
+        private static Product GetProductA() {
+            Sale sale = new Sale();
+            sale.Product = new Product();
+            sale = null;
+            return sale?.Product; //null条件演算子
+        }
+    }
+
+    class Sale {
+        public string ShopName { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
     }
 }
