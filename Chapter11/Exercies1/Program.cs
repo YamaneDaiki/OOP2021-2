7258b1871327fa8a586ff569947aa8138d0e4354
虎太郎 澁澤 <32236@ojs.ac.jp>
2021-08-27 15:20:23 +0900

    1.1

---
+++ Chapter11/Exercies1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercies1 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "Sample.xml";
+            Exercies1_1(file);
+            Console.WriteLine("----------");
+
+            Exercies1_2(file);
+            Console.WriteLine("----------");
+
+            Exercies1_3(file);
+            Console.WriteLine("----------");
+
+        }
+
+        private static void Exercies1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var sample1 = xdoc.Root.Elements();
+            foreach (var sample in sample1) {
+                var xname = sample.Element("name");
+                var team = sample.Element("teammembers");
+                Console.WriteLine("{0},{1}", xname.Value, team.Value);
+            }
+        }
+
+        private static void Exercies1_2(string file) {
+            var xdoc = XDocument.Load(file);
+        }
+
+        private static void Exercies1_3(string file) {
+            var xdoc = XDocument.Load(file);
+        }
+    }
+}
+
+
--- Chapter11/Section01/Program.cs
+++ Chapter11/Section01/Program.cs
@@ -5,17 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var xdoc = XDocument.Load("novelists.xml");
-            foreach (var xnovelist in xdoc.Root.Elements()) {
 
-                var xname = xnovelist.Element("name");
-                var works = xnovelist.Element("masterpieces").Elements("title").Select(x => x.Value);
 
-                Console.WriteLine("{0} - {1}", xname.Value, string.Join(",", works));
+
+            var novelists = ReadNovelists();
+
+
+
+            foreach (var xnovelist in novelists) {
+                Console.WriteLine("{0} ({1} - {2}) - {3}", xnovelist.Name,
+                                    xnovelist.Birth.Year, xnovelist.Death.Year,
+                                    string.Join(",", xnovelist.Masterpieces));
             }
+
+
+
+
         }
+
+
+
+        //カスタムクラスのオブジェクトとして要素を取り出す
+        static public IEnumerable<Novelist> ReadNovelists() {
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                          .Select(x => new Novelist {
+                              Name = (string)x.Element("name"),
+                              KanaName = (string)(x.Element("name").Attribute("kana")),
+                              Birth = (DateTime)x.Element("birth"),
+                              Death = (DateTime)x.Element("death"),
+                              Masterpieces = x.Element("masterpieces")
+                                              .Elements("title")
+                                              .Select(title => title.Value)
+                                              .ToArray()
+                          });
+            return novelists.ToArray();
+
+
+
+        }
+
+
+
     }
 }
