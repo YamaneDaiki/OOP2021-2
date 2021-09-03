5b93023b1d867dcdeb7442fe09e4d19f7e9a9c86
虎太郎 澁澤 <32236@ojs.ac.jp>
2021-09-03 15:13:22 +0900

    kadai

--- Chapter14/Section00/Program.cs
+++ Chapter14/Section00/Program.cs
@@ -8,34 +8,81 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+
 namespace Section04 {
     class Program {
         static void Main(string[] args) {
             new Program();
-
-
         }
 
+
+
+        //コンストラクタ
         public Program() {
-            //DownloadString()
-            // DownloadFileAsync();
-            // OpenReadSample();
-            var results = GetWeatherReportFromYahoo(4610);
+
+            Console.WriteLine("地域コード入力");
+            Console.WriteLine("");
+            Console.WriteLine("1:前橋");
+            Console.WriteLine("");
+            Console.WriteLine("2:みなかみ");
+            Console.WriteLine("");
+            Console.WriteLine("3:宇都宮");
+            Console.WriteLine("");
+            Console.WriteLine("4:水戸");
+            Console.WriteLine("");
+            Console.WriteLine("9:その他（直接入力");
+
+
+            Console.Write(">");
+            int num = int.Parse(Console.ReadLine());
+
+
+
+            switch (num) {
+                case 1:
+                    num = 4210;
+                    break;
+                case 2:
+                    num = 4220;
+                    break;
+                case 3:
+                    num = 4110;
+                    break;
+                case 4:
+                    num = 4010;
+                    break;
+                case 9:
+                    Console.WriteLine("コードを入力して下さい");
+                    Console.Write(">");
+                    num = int.Parse(Console.ReadLine());
+                    break;
+                default:
+                    Console.WriteLine("正しく入力して下さい");
+                    new Program();
+                    break;
+            }
+
+
+            var results = GetWeatherReportFromYahoo(num);
             foreach (var s in results) {
                 Console.WriteLine(s);
             }
-            Console.ReadLine();//入力待ち
         }
 
+
+
+        //リスト14.15
         public void DownloadString() {
             var wc = new WebClient();
             wc.Encoding = Encoding.UTF8;
-            var html = wc.DownloadString("https://yahoo.co.jp/");
+            var html = wc.DownloadString("https://www.yahoo.co.jp/");
             Console.WriteLine(html);
         }
 
 
-        //リスト14.17(非同期処理)
+
+        //リスト14.17
         private void DownloadFileAsync() {
             var wc = new WebClient();
             var url = new Uri(@"C:\Users\ks32236\Desktop\OneDrive_2021-06-22");
@@ -43,8 +90,10 @@ namespace Section04 {
             wc.DownloadProgressChanged += wc_DownloadProgressChanged;
             wc.DownloadFileCompleted += wc_DownloadFileCompleted;
             wc.DownloadFileAsync(url, filename);
-            Console.ReadLine();
+            Console.ReadLine();//アプリケーションが終了しないようにする
         }
+
+
 
         static void wc_DownloadProgressChanged(object sender,
                             DownloadProgressChangedEventArgs e) {
@@ -52,13 +101,16 @@ namespace Section04 {
                               e.BytesReceived, e.TotalBytesToReceive);
         }
 
+
+
         static void wc_DownloadFileCompleted(object sender,
                             System.ComponentModel.AsyncCompletedEventArgs e) {
             Console.WriteLine("ダウンロード完了");
         }
 
 
-        //リスト14.18(ストリームとしてダウンロード)
+
+        //リスト14.18
         public void OpenReadSample() {
             var wc = new WebClient();
             using (var stream = wc.OpenRead("https://yahoo.co.jp/"))
@@ -66,10 +118,11 @@ namespace Section04 {
                 string html = sr.ReadToEnd();
                 Console.WriteLine(html);
             }
-
-
         }
 
+
+
+        //リスト14.19
         private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
             using (var wc = new WebClient()) {
                 wc.Headers.Add("Content-type", "charset=UTF-8");
@@ -77,6 +130,8 @@ namespace Section04 {
                     @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
                 var url = new Uri(uriString);
                 var stream = wc.OpenRead(url);
+
+
 
                 XDocument xdoc = XDocument.Load(stream);
                 var nodes = xdoc.Root.Descendants("title");
@@ -86,6 +141,5 @@ namespace Section04 {
                 }
             }
         }
-
     }
 }
